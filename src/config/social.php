fb5305569ee16868696171848855c77a22fba44e
Евгений Фуреев <efureev@yandex.ru>
2019-07-10 11:53:38 +0500

    fix

--- src/config/social.php
+++ src/config/social.php
@@ -1,5 +1,5 @@
 <?php
-
+use \Fureev\Social\Services\SocialAccountService;
 return [
     'redirectOnAuth' => '/',
     'routes'         => [
@@ -7,6 +7,12 @@ return [
         'redirect'   => '/auth/redirect',
         'middleware' => ['web']
     ],
+    'onSuccess' => static function ($driver) {
+        $user = SocialAccountService::setOrGetUser($driver);
+
+        return SocialAccountService::auth($user);
+    },
+
 
     'drivers' => []
 ];
